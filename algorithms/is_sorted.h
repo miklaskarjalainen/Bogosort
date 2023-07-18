85d3868c207bab1aab866249337cd3004217e999
miklaskarjalainen <miklas.karjalainen@gmail.com>
2023-07-18 11:15:25 +0300

    sort

--- algorithms/is_sorted.h
+++ algorithms/is_sorted.h
@@ -4,7 +4,7 @@ namespace Algorithms {
     template<typename ArrayType, typename Func> 
     bool is_sorted(const ArrayType& array, Func&& cmp) {
         for (int i = 1; i < array.size(); i++) {
-            if (cmp(array[i - 1], array[i]) > 0) {
+            if (cmp(array[i - 1], array[i])) {
                 return false;
             }
         }
