27eac8a964720b68f31eef53accb4143348d56cf
miklaskarjalainen <miklas.karjalainen@gmail.com>
2023-07-18 11:15:19 +0300

    'PCG' random generator

--- randomizer/random.h
+++ randomizer/random.h
@@ -1,30 +1,54 @@
 #pragma once
 
+typedef unsigned int       uint32_t;
+typedef unsigned long long uint64_t;
+
 namespace Randomizer {
+
+	// based on: https://en.wikipedia.org/wiki/Permuted_congruential_generator
 	class Random {
 	public:
 		Random() {};
 		~Random() {};
 		
 		void set_seed(unsigned int seed) {
-			m_Seed = seed;
+			m_Seed = seed + Increment;
+			rand();
 		}
-		// https://stackoverflow.com/questions/6417113/c-custom-random-function
-		int rand() {
-			m_Seed = m_Seed * 1664525 + 1013904223;
-   			return m_Seed >> 24;
+
+		uint32_t rand()
+		{
+			uint64_t x = m_Seed;
+			unsigned count = (unsigned)(x >> 59);		// 59 = 64 - 5
+
+			m_Seed = x * Multiplier + Increment;
+			x ^= x >> 18;								// 18 = (64 - 27)/2
+			return rotr32((uint32_t)(x >> 27), count) % RandMax;	// 27 = 32 - 5
 		}
+
+		float randf() {
+			return ((float)rand() / RandMax);
+		}
+
         // (inclusive)
 		// https://stackoverflow.com/questions/12657962/how-do-i-generate-a-random-number-between-two-variables-that-i-have-stored
-        int rand_range(const int& min, const int& max) { return rand() % (max-min+1) + 1; }
+        int rand_range(const int& min, const int& max) {
+			return (int)rand()%(max-min+1)+min;
+		}
+
         float rand_range(float min, float max) {
-            const float MaxFloatNumber = 40.0000000000000000;
-            return ((float)rand() / MaxFloatNumber) * (max - min) + min;
+            return randf() * (max - min) + min;
         }
 
-        
-
+		const uint32_t RandMax = 0x7FFF;
 	private:
-		unsigned int m_Seed = 1664525;
+		static uint32_t rotr32(uint32_t x, unsigned r)
+		{
+			return x >> r | x << (-r & 31);
+		}
+		
+		uint64_t m_Seed = 0x4d595df4d0f33173;
+		const uint64_t Multiplier = 6364136223846793005u;
+		const uint64_t Increment  = 1442695040888963407u;
 	};
 }
